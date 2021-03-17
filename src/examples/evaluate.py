38c7acbafa56d825f92000f4a640de2b16bdc7e0
dwoiwode <dominik.woiwode@stud.uni-hannover.de>
2021-03-17 10:04:34 +0100

    Refactor training/evaluation methods

---
+++ src/examples/evaluate.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+from stable_baselines3.common.base_class import BaseAlgorithm
+import logging
+import tqdm
+
+
+def evaluate_model(env, savepoint="random", render=True, iteration=0, plot_results="save"):
+    logger = logging.getLogger(f"Eval[{savepoint}:{iteration}]")
+    logger.info("")
+    logger.info("=" * 120)
+    logger.info(f"{f'Savepoint: {savepoint}, reward_type: {env.reward_type}':=^120}")
+    # logger.info("="*5,f"Savepoint: {savepoint}, reward_type: {env.reward_type}", "="*5)
+    logger.info("=" * 120)
+
+    if savepoint not in ["random", "argmax", None]:
+        model = BaseAlgorithm.load(savepoint)
+
+    # Evaluation
+    t = []
+    velocities = []
+    rewards = []
+    actions = []
+
+    observation = env.reset()
+    progress = tqdm.tqdm(total=env.horizon)
+    done = False
+    while not done:
+        progress.update(1)
+        # Get Action
+        if savepoint.startswith("random"):
+            action = env.action_space.sample()
+        elif savepoint.startswith("argmax"):
+            action = np.argmax(observation)
+        else:
+            action, _ = model.predict(observation)
+
+        actions.append(action)
+        observation, reward, done, info = env.step(action)
+        logger.debug(f"Action: {action}")
+        logger.debug(f"Observation: {observation}")
+        logger.debug(f"Reward: {reward}")
+        if render:
+            env.render()
+
+        velocities.append(env.world.mean_velocity)
+        t.append(env.world.t)
+        rewards.append(reward)
+
+    progress.close()
+
+    # Plotting diagrams
+    if plot_results is not None:
+        plt.plot(t, velocities, ".", label="Velocity")
+        plt.plot(t, rewards, ".", label=f"Reward ({env.reward_type})")
+        plt.ylim(-3, 16)
+        plt.legend()
+        plt.title(f"{savepoint}")
+        plt.tight_layout()
+        if plot_results == "show":
+            plt.show()
+        elif plot_results == "save":
+            plt.savefig(f"figures/{Path(savepoint).name}_{iteration}.png")
+        plt.close()
+
+    print("Actions:", np.unique(actions, return_counts=True))
+    print("Mean vel:", np.mean(velocities))
+    print("Std vel:", np.std(velocities))
+    print("Sum reward:", np.sum(rewards))
+    return np.mean(velocities), np.std(velocities), np.mean(rewards), np.std(rewards), np.sum(rewards)
+
+
+def evaluate_folder(savepoint_folder="", iteration=0):
+    resultFile = Path("results_conventional.csv")
+    if not resultFile.exists():
+        with resultFile.open("w") as d:
+            d.write("filename,vel_mean,vel_std,rew_mean,rew_std,rew_sum,iteration\n")
+    for savepoint in Path(savepoint_folder).iterdir():
+        values = evaluate_model(env, str(savepoint), render=False, iteration=iteration)
+        with resultFile.open("a") as d:
+            d.write(",".join(map(str, [savepoint] + list(values) + [iteration])) + "\n")
+        print(savepoint)
+
+
+if __name__ == '__main__':
+    from gymTraffic.TrafficGym import TrafficGym, TrafficGymMeta
+    import worlds.savepoints as save
+
+    # env = lambda: TrafficGym(save.graph_3x3circle, horizon=1000, reward_type="acceleration", action_frequency=1)
+    env = lambda: TrafficGymMeta(save.graph_3x3circle, horizon=1000, reward_type="acceleration", action_frequency=1)
+
+    evaluate_model(env(), "random")
---
+++ src/examples/train.py
@@ -0,0 +1,90 @@
+import time
+from typing import Optional
+
+from gym.wrappers import Monitor
+from stable_baselines3 import PPO
+from stable_baselines3.common.base_class import BaseAlgorithm
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import SubprocVecEnv
+
+
+
+def train(env_function, name="model", n_processes: int = 6, seed: int = 0, load_checkpoint: Optional[str] = None,
+          from_index=0, to_index=12, steps_per_episode=125*1000):
+    """
+    Trains a model with a given environment
+
+    :param env_function: Function that creates an gym.Env
+    :param name: name for saving
+    :param n_processes: number of processes used for training
+    :param seed:
+    :param load_checkpoint: if None: Create new model. Else: Load model from file
+    :return:
+    """
+
+    def make_env(rank:int):
+        """
+        Utility function for multiprocessed env.
+
+        :param rank: index of the subprocess (needed to update seed)
+        """
+
+        def _init():
+            env = env_function()
+            # Important: use a different seed for each environment
+            env.seed(seed + rank)
+            return env
+
+        return _init
+
+    # Create the vectorized environment
+    env_vector = SubprocVecEnv([make_env(i) for i in range(n_processes)])
+
+    # Create model
+    if load_checkpoint is None:
+        model = PPO(
+            "MlpPolicy",
+            env_vector,
+            tensorboard_log="./ppo_trafficgym_tensorboard/",
+            verbose=2,
+            learning_rate=1e-2,
+            # gamma=0.95,
+            batch_size=256,
+            policy_kwargs=dict(net_arch=[64, 64]),
+        )
+    else:
+        model = BaseAlgorithm.load(load_checkpoint)
+
+    # Evaluate before training
+    env = Monitor(env_function())
+    print("Evaluating...")
+    evaluation = evaluate_policy(model, env)
+    print("Eval1:", evaluation)
+
+    # Actual training
+    t1 = time.time()
+    for i in range(from_index, to_index):
+        try:
+            model.learn(steps_per_episode)
+            print(f"Save model {i}")
+            model.save(f"{name}{i:02d}.stable_baselines")
+        except KeyboardInterrupt:
+            print("Interrupted by KeyBoard")
+            break
+    t2 = time.time()
+    print(f"Learning took {t2 - t1} seconds")
+
+    # Evaluate after training
+    print("Evaluating...")
+    evaluation = evaluate_policy(model, env)
+    print("Eval2:", evaluation)
+
+
+if __name__ == '__main__':
+    from gymTraffic.TrafficGym import TrafficGym, TrafficGymMeta
+    import worlds.savepoints as save
+
+    # env_function = lambda: TrafficGym(save.graph_3x3circle, horizon=1000, reward_type="acceleration")
+    env_function = lambda: TrafficGymMeta(save.graph_3x3circle, horizon=1000, reward_type="acceleration")
+
+    train(env_function, "ppo_meta")
